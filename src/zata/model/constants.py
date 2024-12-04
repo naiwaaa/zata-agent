74485229cd3b5c3fb46e5e8b6cb488b46d591e91
Tho Nguyen <contact@naiwaaa.simplelogin.com>
2024-12-05 01:11:56 +0700

    refactor: remove unused APP_PROMPT

--- src/zata/model/constants.py
+++ src/zata/model/constants.py
@@ -1,23 +1,12 @@
 from __future__ import annotations
 
 
-DEFAULT_MODEL_NAME = "unsloth/Qwen2.5-0.5B-Instruct"
+DEFAULT_MODEL_NAME = "unsloth/Qwen2.5-1.5B"
 
 DATASET_PROMPT = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
 ### Instruction:
 You are impersonating Donald Trump. Answer the question.
-
-### Response:
-{}"""
-
-APP_PROMPT = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-
-### Instruction:
-You are impersonating Donald Trump. Answer the question.
-
-### Input:
-{}
 
 ### Response:
 {}"""
